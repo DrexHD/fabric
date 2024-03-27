21e2e22705951aa44aeb198a60d899c6ca8a52f0
Drex <nicknamedrex@gmail.com>
2024-03-27 19:36:56 +0100

    Revert variants MapCodec to Codec

--- fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/api/transfer/v1/storage/base/SingleVariantStorage.java
+++ fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/api/transfer/v1/storage/base/SingleVariantStorage.java
@@ -174,9 +174,9 @@ public abstract class SingleVariantStorage<T extends TransferVariant<?>> extends
 	 * @param wrapperLookup the {@link RegistryWrapper.WrapperLookup} instance
 	 * @param <T> the type of the item variant
 	 */
-	public static <T extends TransferVariant<?>> void readNbt(SingleVariantStorage<T> storage, MapCodec<T> codec, Supplier<T> fallback, NbtCompound nbt, RegistryWrapper.WrapperLookup wrapperLookup) {
+	public static <T extends TransferVariant<?>> void readNbt(SingleVariantStorage<T> storage, Codec<T> codec, Supplier<T> fallback, NbtCompound nbt, RegistryWrapper.WrapperLookup wrapperLookup) {
 		final RegistryOps<NbtElement> ops = wrapperLookup.getOps(NbtOps.INSTANCE);
-		final DataResult<T> result = codec.codec().parse(ops, nbt.getCompound("variant"));
+		final DataResult<T> result = codec.parse(ops, nbt.getCompound("variant"));
 
 		if (result.error().isPresent()) {
 			LOGGER.debug("Failed to load an ItemVariant from NBT: {}", result.error().get());
@@ -197,9 +197,9 @@ public abstract class SingleVariantStorage<T extends TransferVariant<?>> extends
 	 * @param wrapperLookup the {@link RegistryWrapper.WrapperLookup} instance
 	 * @param <T> the type of the item variant
 	 */
-	public static <T extends TransferVariant<?>> void writeNbt(SingleVariantStorage<T> storage, MapCodec<T> codec, NbtCompound nbt, RegistryWrapper.WrapperLookup wrapperLookup) {
+	public static <T extends TransferVariant<?>> void writeNbt(SingleVariantStorage<T> storage, Codec<T> codec, NbtCompound nbt, RegistryWrapper.WrapperLookup wrapperLookup) {
 		final RegistryOps<NbtElement> ops = wrapperLookup.getOps(NbtOps.INSTANCE);
-		nbt.put("variant", codec.codec().encode(storage.variant, ops, nbt).getOrThrow(RuntimeException::new));
+		nbt.put("variant", codec.encode(storage.variant, ops, nbt).getOrThrow(RuntimeException::new));
 		nbt.putLong("amount", storage.amount);
 	}
 }
